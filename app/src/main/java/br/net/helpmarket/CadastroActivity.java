71d1d54167a6d5f304deb1fccc6d4dd66895af8d
eduardoar <eduardoazvd17@gmail.com>
2019-07-03 12:26:15 -0300

    Initial commit

---
+++ app/src/main/java/br/net/helpmarket/CadastroActivity.java
@@ -0,0 +1,13 @@
+package br.net.helpmarket;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
