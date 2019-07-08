4928c9eccfb273c28102de37e3e37721a3c9b293
eduardoar <eduardoazvd17@gmail.com>
2019-07-08 14:30:08 -0300

    Criação das telas.

---
+++ app/src/main/java/br/net/helpmarket/InformacoesActivity.java
@@ -0,0 +1,74 @@
+package br.net.helpmarket;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class InformacoesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grupos);
+
+        toolbar = findViewById(R.id.info_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawerLayout = findViewById(R.id.info_drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorWhite));
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_inicio: {
+
+                break;
+            }
+            case R.id.nav_listaCompras: {
+
+                break;
+            }
+            case R.id.nav_grupos: {
+
+                break;
+            }
+            case R.id.nav_informacoes: {
+
+                break;
+            }
+            case R.id.nav_configuracoes: {
+
+                break;
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            return;
+        }
+    }
+}
